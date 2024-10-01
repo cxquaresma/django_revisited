cc958ceff6fd39b13c55ae0d7c8ea7ca2762b71b
cxquaresma <57335050+cxquaresma@users.noreply.github.com>
2024-10-01 16:55:45 -0400

    2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-p633=6f5ko#d3l6sk7*--6*8_ps8u45a+%20x*&5l0y#2v1wjy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
